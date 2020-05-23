bac8a09300d4deed6600d5a399e6615a56285303
Ryota Saito <sa110.instagram@gmail.com>
2020-05-23 18:04:26 +0900

    基本レイアウトの作成

--- App.js
+++ App.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
-  );
-}
-
+// Styleは上に記述する流派と下に書く流派がある。
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -16,4 +9,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row', //rowが横並び colunmが縦並び
+  },
+  leftContainer: {
+    backgroundColor: 'red',
+    width: 100,
+  },
+  rightContainer: {
+    backgroundColor: 'blue',
+    flex: 1, //余ったエリアを全て使って描画を行う
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer} />
+        <View style={styles.rightContainer} />
+      </View>
+    </View>
+  );
+}
